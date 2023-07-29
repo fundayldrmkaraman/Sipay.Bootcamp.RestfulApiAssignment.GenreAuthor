46a500dc1e1040a01bef99fcfbd461cc1ebbc5cb
Funda Yıldırımkaraman <108402804+fundayldrmkaraman@users.noreply.github.com>
2023-07-29 19:20:20 +0300

    Add files via upload

---
+++ Sipay.Bootcamp.RestfulApiAssignmentGenreAuthor/GenreBookTests/BookServiceTests.cs
@@ -0,0 +1,117 @@
+﻿using Moq;
+using RestfulApiAssignment.Books;
+using RestfulApiAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GenreBookTests
+{
+    public class BookServiceTests
+    {
+        private readonly List<Book> _books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Book 1", Author = "Author 1" },
+            new Book { Id = 2, Title = "Book 2", Author = "Author 2" },
+            new Book { Id = 3, Title = "Book 3", Author = "Author 1" }
+        };
+
+        [Fact]
+        public void GetAllBooks_ShouldReturnAllBooks()
+        {
+           var mockBookService = new Mock<IBookService>();
+            mockBookService.Setup(service => service.GetAllBooks()).Returns(_books);
+
+            var bookService = mockBookService.Object;
+            var result = bookService.GetAllBooks();
+
+            Assert.Equal(_books.Count, result.Count);
+            Assert.Equal(_books, result);
+        }
+
+        [Fact]
+        public void GetBookById_ValidId_ShouldReturnCorrectBook()
+        {
+            var bookId = 2;
+            var book = _books.FirstOrDefault(b => b.Id == bookId);
+
+            var mockBookService = new Mock<IBookService>();
+            mockBookService.Setup(service => service.GetBookById(bookId)).Returns(book);
+
+            var bookService = mockBookService.Object;
+
+            var result = bookService.GetBookById(bookId);
+
+            Assert.NotNull(result);
+            Assert.Equal(bookId, result.Id);
+        }
+
+        [Fact]
+        public void GetBookById_InvalidId_ShouldReturnNull()
+        {
+            var bookId = 100;
+
+            var mockBookService = new Mock<IBookService>();
+            mockBookService.Setup(service => service.GetBookById(bookId)).Returns((Book)null);
+
+            var bookService = mockBookService.Object;
+
+            var result = bookService.GetBookById(bookId);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void CreateBook_ValidBook_ShouldAddToBookList()
+        {
+            var newBook = new Book { Id = 4, Title = "New Book", Author = "New Author" };
+
+            var mockBookService = new Mock<IBookService>();
+            mockBookService.Setup(service => service.CreateBook(newBook));
+
+            var bookService = mockBookService.Object;
+
+            bookService.CreateBook(newBook);
+
+            Assert.Contains(newBook, _books);
+        }
+
+        [Fact]
+        public void UpdateBook_ValidBook_ShouldUpdateBookDetails()
+        {
+            var bookId = 2;
+            var updatedBook = new Book { Id = bookId, Title = "Updated Book", Author = "Updated Author" };
+
+            var mockBookService = new Mock<IBookService>();
+            mockBookService.Setup(service => service.UpdateBook(bookId, updatedBook));
+
+            var bookService = mockBookService.Object;
+
+            bookService.UpdateBook(bookId, updatedBook);
+
+            var book = _books.FirstOrDefault(b => b.Id == bookId);
+            Assert.NotNull(book);
+            Assert.Equal(updatedBook.Title, book.Title);
+            Assert.Equal(updatedBook.Author, book.Author);
+        }
+
+        [Fact]
+        public void DeleteBook_ValidId_ShouldRemoveBookFromList()
+        {
+            var bookId = 3;
+
+            var mockBookService = new Mock<IBookService>();
+            mockBookService.Setup(service => service.DeleteBook(bookId));
+
+            var bookService = mockBookService.Object;
+
+            bookService.DeleteBook(bookId);
+
+            var book = _books.FirstOrDefault(b => b.Id == bookId);
+            Assert.Null(book);
+        }
+    }
+}
---
+++ Sipay.Bootcamp.RestfulApiAssignmentGenreAuthor/GenreBookTests/GenreServiceTests.cs
@@ -0,0 +1,117 @@
+﻿using Moq;
+using RestfulApiAssignment.GenreService;
+using RestfulApiAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GenreBookTests
+{
+    public class GenreServiceTests
+    {
+        private readonly List<Genre> _genres = new List<Genre>
+        {
+            new Genre { Id = 1, Name = "Fiction" },
+            new Genre { Id = 2, Name = "Fantasy" },
+            new Genre { Id = 3, Name = "Mystery" }
+        };
+
+        [Fact]
+        public void GetAllGenres_ShouldReturnAllGenres()
+        {
+            var mockGenreService = new Mock<IGenreService>();
+            mockGenreService.Setup(service => service.GetAllGenres()).Returns(_genres);
+
+            var genreService = mockGenreService.Object;
+
+            var result = genreService.GetAllGenres();
+
+            Assert.Equal(_genres.Count, result.Count);
+            Assert.Equal(_genres, result);
+        }
+
+        [Fact]
+        public void GetGenreById_ValidId_ShouldReturnCorrectGenre()
+        {
+            var genreId = 2;
+            var genre = _genres.FirstOrDefault(g => g.Id == genreId);
+
+            var mockGenreService = new Mock<IGenreService>();
+            mockGenreService.Setup(service => service.GetGenreById(genreId)).Returns(genre);
+
+            var genreService = mockGenreService.Object;
+
+            var result = genreService.GetGenreById(genreId);
+
+            Assert.NotNull(result);
+            Assert.Equal(genreId, result.Id);
+        }
+
+        [Fact]
+        public void GetGenreById_InvalidId_ShouldReturnNull()
+        {
+            var genreId = 100;
+
+            var mockGenreService = new Mock<IGenreService>();
+            mockGenreService.Setup(service => service.GetGenreById(genreId)).Returns((Genre)null);
+
+            var genreService = mockGenreService.Object;
+
+            var result = genreService.GetGenreById(genreId);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void CreateGenre_ValidGenre_ShouldAddToGenreList()
+        {
+            var newGenre = new Genre { Id = 4, Name = "Science Fiction" };
+
+            var mockGenreService = new Mock<IGenreService>();
+            mockGenreService.Setup(service => service.CreateGenre(newGenre));
+
+            var genreService = mockGenreService.Object;
+
+            genreService.CreateGenre(newGenre);
+
+            Assert.Contains(newGenre, _genres);
+        }
+
+        [Fact]
+        public void UpdateGenre_ValidGenre_ShouldUpdateGenreName()
+        {
+            var genreId = 2;
+            var updatedGenre = new Genre { Id = genreId, Name = "Updated Genre" };
+
+            var mockGenreService = new Mock<IGenreService>();
+            mockGenreService.Setup(service => service.UpdateGenre(genreId, updatedGenre));
+
+            var genreService = mockGenreService.Object;
+
+            genreService.UpdateGenre(genreId, updatedGenre);
+
+            var genre = _genres.FirstOrDefault(g => g.Id == genreId);
+            Assert.NotNull(genre);
+            Assert.Equal(updatedGenre.Name, genre.Name);
+        }
+
+        [Fact]
+        public void DeleteGenre_ValidId_ShouldRemoveGenreFromList()
+        {
+            var genreId = 3;
+
+            var mockGenreService = new Mock<IGenreService>();
+            mockGenreService.Setup(service => service.DeleteGenre(genreId));
+
+            var genreService = mockGenreService.Object;
+
+            genreService.DeleteGenre(genreId);
+
+            var genre = _genres.FirstOrDefault(g => g.Id == genreId);
+            Assert.Null(genre);
+        }
+    }
+}
